6481ff810eac6daf79ae87af94283dfac4eb3179
noval oktavio <novaloktavio@gmail.com>
2019-04-11 22:40:14 +0700

    header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="col-12 pt-5">
+      <h2>N O V A L &nbsp&nbspO K T A V I O</h2>
     </div>
+    <nav class="navbar navbar-expand-lg sticky-top">
+      <!-- <a class="navbar-brand" href="#">Photonesia</a> -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav" style="justify-content: center;">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GALLERY</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">ABOUT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
+.navbar{
+  background: #fff;
+}
+.navbar ul li a{
+  color: #2c3e50;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
