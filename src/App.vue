8c13173760c9a36085d966c1cd9f605b7796caf9
noval oktavio <novaloktavio@gmail.com>
2019-04-12 23:31:28 +0700

    content homepage

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div class="col-12 pt-5">
-      <h2>N O V A L &nbsp;&nbsp;O K T A V I O</h2>
+    <div class="col-12 pt-2">
+      <h2 style="font-size:4vh !important;">N O V A L &nbsp;&nbsp;O K T A V I O</h2>
     </div>
-    <nav class="navbar navbar-expand-lg sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top">
       <!-- <a class="navbar-brand" href="#">Photonesia</a> -->
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -23,6 +23,20 @@
       </div>
     </nav>
     <router-view/>
+    <div class="footer row p-3">
+      <div class="col-xs-4 col-md-4 col-lg-4 px-4 mt-4 text-left">
+       <h6><b>ABOUT ME</b></h6>
+        <span style="font-size:14px">My name is Noval Oktavio. I'm a photography hobbyst. <br>I'm from Bandung, Indonesia.</span>
+      </div>
+      <div class="col-xs-4 col-md-4 col-lg-4 px-4 mt-4 text-left">
+       <h6><b>CONTACT</b></h6>
+        <span style="font-size:14px;color: #e2e2e2;">081214845674</span><br>
+        <span style="font-size:14px;color: #e2e2e2;">novaloktavio@gmail.com</span>
+      </div>
+      <div class="col-xs-4 col-md-4 col-lg-4 px-4 mt-4 text-left">
+        <h6><b>FIND ME</b></h6>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,11 +47,17 @@
 .navbar ul li a{
   color: #2c3e50;
 }
+.footer{
+  background: #232323;
+  color: #fff;
+  min-height: 150px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 15px 15px 0 15px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,210 @@
 <template>
   <div class="home">
-    <carousel :per-page="3" autoplay="true" loop="true">
-      <slide>
-        <img style="width:100%" src="../../public/image/gallery15.jpg" alt="">
+    <carousel :per-page="1" :autoplay="true" :loop="true">
+      <slide style="max-height:450px">
+        <img style="height:100%" src="../../public/image/slider.jpg" alt="">
       </slide>
-      <slide>
-        <img style="width:100%" src="../../public/image/gallery1.jpg" alt="">
+      <slide style="max-height:450px">
+        <img style="height:100%" src="../../public/image/gallery1.jpg" alt="">
       </slide>
-      <slide>
-        <img style="width:100%" src="../../public/image/gallery7.jpg" alt="">
+      <slide style="max-height:450px">
+        <img style="height:100%" src="../../public/image/gallery7.jpg" alt="">
       </slide>
-      <slide>
-        <img style="width:100%" src="../../public/image/gallery13.jpg" alt="">
+      <slide style="max-height:450px">
+        <img style="height:100%" src="../../public/image/gallery13.jpg" alt="">
       </slide>
-      <slide>
-        <img style="width:100%" src="../../public/image/gallery12.jpeg" alt="">
+      <slide style="max-height:450px">
+        <img style="height:100%" src="../../public/image/gallery12.jpeg" alt="">
       </slide>
-      <slide>
-        <img style="width:100%" src="../../public/image/gallery14.jpg" alt="">
+      <slide style="max-height:450px">
+        <img style="height:100%" src="../../public/image/gallery14.jpg" alt="">
       </slide>
     </carousel>
+    <div class="row p-4 my-5">
+      <div class="col-12">
+        <h3>T H I S &nbsp;&nbsp;I S &nbsp;&nbsp;M E</h3>
+        <p>I love photography since 2013, but my first DSLR Camera in 2018.<br />
+          I like to take any photos like portraits, minimalism, landscape, street, etc.
+        </p>
+      </div>
+      <div class="col-12 mt-3">
+        <a class="btn-see" href="#">SEE MY GALLERY</a>
+      </div>
+    </div>
+    <div class="row my-work mb-5">
+      <div class="col-12 my-4">
+      <hr />
+        <h3>M Y &nbsp;&nbsp;R E C E N T &nbsp;&nbsp;W O R K</h3>
+      <hr />
+      </div>
+      <!-- <div class="col-12 card-columns">
+        <div class="card card-pin">
+          <img class="card-img"
+            src="../../public/image/gallery12.jpg"
+            alt="Card image">
+          <div class="overlay">
+            <h3 class="card-title title">Some Title</h3>
+            <div class="download">
+              <a href="http://lorempixel.com/486/320/" download>
+                <i class="fa fa-download" aria-hidden="true"></i>
+              </a>
+            </div>
+            <div class="more">
+              <a href="#!">
+                <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> More
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="card card-pin">
+          <img class="card-img"
+            src="../../public/image/gallery1.jpg"
+            alt="Card image">
+          <div class="overlay">
+            <h3 class="card-title title">Some Title</h3>
+            <div class="more">
+              <a href="#!">
+                <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> More
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="card card-pin">
+          <img class="card-img"
+            src="../../public/image/gallery17.jpg"
+            alt="Card image">
+          <div class="overlay">
+            <h3 class="card-title title">Some Title</h3>
+            <div class="more">
+              <a href="#!">
+                <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> More
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="card card-pin">
+          <img class="card-img"
+            src="../../public/image/gallery16.jpg"
+            alt="Card image">
+          <div class="overlay">
+            <h3 class="card-title title">Some Title</h3>
+            <div class="more">
+              <a href="#!">
+                <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div> -->
+      <div class="col-xs-4 col-md-4 mb-4">
+        <img width="100%" src="../../public/image/gallery1.jpg">
+      </div>
+      <div class="col-xs-4 col-md-4 mb-4">      
+        <img width="100%" src="../../public/image/gallery7.jpg">
+      </div>
+      <div class="col-xs-4 col-md-4 mb-4">
+        <img width="100%" src="../../public/image/gallery10.jpg">
+      </div>
+      <div class="col-xs-4 col-md-4 mb-4">
+        <img width="100%" src="../../public/image/gallery13.jpg">
+      </div>
+      <div class="col-xs-4 col-md-4 mb-4">
+        <img width="100%" src="../../public/image/gallery11.jpg">
+      </div>
+      <div class="col-xs-4 col-md-4 mb-4">
+        <img width="100%" src="../../public/image/gallery.jpg">
+      </div>
+      <div class="col-12 mt-3">
+        <a class="btn-see" href="#">SEE MORE</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+  export default {}
 </script>
+
+<style>
+  .btn-see {
+    background: #232323;
+    color: #fff;
+    padding: 14px;
+    border-radius: 50px;
+    text-decoration: none;
+    font-size: 14px;
+    transition: ease .5s;
+    border: 1px solid #232323
+  }
+  .btn-see:hover{
+    color: #232323;
+    border: 1px solid #232323;
+    background: #ffffff;
+    transition: ease .5s;
+    text-decoration: none;
+  }
+  .text-muted a {
+    color: #989898;
+  }
+  .text-muted a:hover {
+    color: grey;
+  }
+  .fa a:active,
+  a:hover,
+  a:visited {
+    text-decoration: none;
+  }
+  .general .card-title {
+    color: #f3f3f3;
+  }
+  .general .card-text {
+    color: #f3f3f3;
+  }
+  .card-img {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+  .overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    transition: .2s ease;
+    background-color: #000;
+  }
+  .card-pin:hover .overlay {
+    opacity: .5;
+    /* border: 5px solid #f3f3f3; */
+    transition: ease .5s;
+    background-color: #000000;
+  }
+  .more {
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    text-transform: uppercase;
+    transform: translate(-20%, -20%);
+    -ms-transform: translate(-50%, -50%);
+  }
+  .card-pin:hover .card-title {
+    color: #ffffff;
+    margin-top: 10px;
+    text-align: center;
+    font-size: 1.2em;
+  }
+  .card-pin:hover .more a {
+    text-decoration: none;
+    color: #ffffff;
+  }
+  .card-pin:hover .download a {
+    text-decoration: none;
+    color: #ffffff;
+  }
+</style>
